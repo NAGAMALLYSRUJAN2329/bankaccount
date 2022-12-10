ce71055c45279513ffeab3cdf1269f379b92a4c2
NAGAMALLYSRUJAN2329 <118573078+NAGAMALLYSRUJAN2329@users.noreply.github.com>
2022-12-10 22:32:11 +0530

    now it is working(game_with_strings)

--- game_with_string.cpp
+++ game_with_string.cpp
@@ -7,21 +7,20 @@ int main(){
     int l;
     l=word.length();
     int i=0;
-
-    for(i;i<l;i++){
+    for(i;i<l-1;i++){
         if(word[i]==word[i+1]){
             a++;
-            for(int j=0;j<=l-i-3;i++){
+            for(int j=0;j<=l-i-3;j++){
                 word[i+j]=word[i+j+2];
-    }i--;
+    }word[l-1]=0;
+    word[l-2]=0;
+    i=i-2;
     l=l-2;
-    }
-    }
+    }}
     if(a%2==0){
         cout<<"No";
     }else{
         cout<<"Yes";
     }
-
     return 0;
 }
